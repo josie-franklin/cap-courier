06a450fcd1c28e675fe3be66ddc1fce162c866e6
dolcebasstrombone <dolcebasstrombone@gmail.com>
2023-06-20 16:17:55 -0400

    add port to server file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
