e2b58f50684f7960e548743637db74a965eee378
dolcebasstrombone <dolcebasstrombone@gmail.com>
2023-07-15 22:31:54 -0400

    Added Auth0 on local.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import CollectionContextProvider from "./context/CollectionContext";
 import Header from "./components/Header";
 import HomePage from "./components/Home/HomePage";
 import CollectionPage from "./components/Collection/CollectionPage";
 import AboutMePage from "./components/AboutMe/AboutMePage";
 import AboutWebsitePage from "./components/AboutWebsite/AboutWebsitePage";
+import Footer from "./components/Footer";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import backgroundPaper from "./images/textured_paper.png";
@@ -21,30 +23,43 @@ const AppContainer = styled(Box)(() => ({
 const App = () => {
   return (
     <AppContainer>
-      <Router>
-        <Header />
-        <Routes>
-          <Route exact key="Home" path="/" element={<HomePage />} />
-          <Route
-            exact
-            key="Collection"
-            path="/collection"
-            element={<CollectionContextProvider><CollectionPage /></CollectionContextProvider>}
-          />
-          <Route
-            exact
-            key="AboutMe"
-            path="/aboutme"
-            element={<AboutMePage />}
-          />
-          <Route
-            exact
-            key="AboutWebsite"
-            path="/aboutwebsite"
-            element={<AboutWebsitePage />}
-          />
-        </Routes>
-      </Router>
+      <Auth0Provider
+        domain="orange-water-6755.us.auth0.com"
+        clientId="9LVxPdpi6EZqsXruueSHvP4sDkwG53vd"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <Router>
+          <Header />
+          <Routes>
+            <Route exact key="Home" path="/" element={<HomePage />} />
+            <Route
+              exact
+              key="Collection"
+              path="/collection"
+              element={
+                <CollectionContextProvider>
+                  <CollectionPage />
+                </CollectionContextProvider>
+              }
+            />
+            <Route
+              exact
+              key="AboutMe"
+              path="/aboutme"
+              element={<AboutMePage />}
+            />
+            <Route
+              exact
+              key="AboutWebsite"
+              path="/aboutwebsite"
+              element={<AboutWebsitePage />}
+            />
+          </Routes>
+          <Footer />
+        </Router>
+      </Auth0Provider>
     </AppContainer>
   );
 };
---
+++ client/src/components/Footer.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+
+const Strip = styled(Box)(({ theme }) => ({
+  width: "100%",
+  justifyContent: "center",
+  borderTop: "var(--thin-border)",
+  display: "flex",
+  [theme.breakpoints.down("md")]: {
+    display: "none",
+  },
+}));
+
+const StyledLink = styled(Box)(() => ({
+  color: "var(--primary)",
+  textDecoration: "none",
+  fontFamily: "Georgia, serif",
+  display: "flex",
+  alignItems: "center",
+  paddingTop: "3px",
+  "&:hover": {
+    cursor: "pointer",
+  },
+}));
+
+const Footer = () => {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
+  useEffect(() => {
+    console.log(isAuthenticated);
+    console.log(user);
+  }, []);
+
+  return (
+    <Box sx={{ padding: "0 40px 20px" }}>
+      <Strip>
+        {!isAuthenticated && (
+          <StyledLink onClick={loginWithRedirect}>
+            Copyright © 2023 The Cap Courier - Design by Josie Franklin
+          </StyledLink>
+        )}
+        {isAuthenticated && (
+          <StyledLink
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Welcome, Josie!
+          </StyledLink>
+        )}
+      </Strip>
+    </Box>
+  );
+};
+
+export default Footer;
