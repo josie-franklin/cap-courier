7e0a5609df2071c37ed66dbf2e071f87c2670003
dolcebasstrombone <dolcebasstrombone@gmail.com>
2025-02-03 19:49:08 -0600

    ... and again

--- server/server.js
+++ server/server.js
@@ -9,9 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 app.use(routes);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
