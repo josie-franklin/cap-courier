e73333634564dfa245bbea21176d0a2de3d2471b
dolcebasstrombone <dolcebasstrombone@gmail.com>
2025-02-03 19:39:40 -0600

    trying new order

--- server/server.js
+++ server/server.js
@@ -9,15 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-// if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-  // });
-// }
 app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}.`));
