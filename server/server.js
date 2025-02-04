6c1580ba99b32e7d544a7eeb341ba58eb8300d66
dolcebasstrombone <dolcebasstrombone@gmail.com>
2025-02-03 18:00:25 -0600

    Added change to server.js to handle react client requests

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
