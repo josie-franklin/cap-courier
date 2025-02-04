f9d5c21c202f5fb91c7dddbc482481815569d947
dolcebasstrombone <dolcebasstrombone@gmail.com>
2025-02-03 18:46:56 -0600

    fix production routing

--- server/server.js
+++ server/server.js
@@ -1,22 +1,23 @@
-const path = require('path');
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/build')));
 
-process.env.NODE_ENV === 'production' && app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    });
+}
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}.`));
 });
-
